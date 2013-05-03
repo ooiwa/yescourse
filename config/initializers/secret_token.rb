53124a383abefbba8e836bafdaed4dd8185cb12b
Yoshiyuki Ohiwa <yoshiyuki.ohiwa@gmail.com>
2013-05-03 21:03:48 +0900

    create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yescourse::Application.config.secret_token = '88837d790d43751dc6729cc5032a6b80e89dd88b822608ea8d59d8877246ee876899e32819808932d5e76bd6ba344659cc60d96a7df1c406154707f2e29c4442'
